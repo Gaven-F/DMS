ea659f3e9563bec26a4172b7a96936b2ba01fc0e
fallen gaven <gavenf2023@outlook.com>
2024-02-22 16:34:26 +0800

    feat: :sparkles: 初始化客户端
    初始化了一个前端的框架；
    创建了git忽略文件；

---
+++ DMS.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<DMS.Client.Components.Routes>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+var app = builder.Build();
+
+builder.Services.AddScoped(sp =>
+{
+	return new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+});
+
+await builder.Build().RunAsync();
