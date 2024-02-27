74255554a920c10335cf770c607044ea3fa0014b
Gaven-F <133743603+Gaven-F@users.noreply.github.com>
2024-02-27 17:38:44 +0800

    Revert "feat: :sparkles: CLIENT | 压缩CSS与JS"

--- DMS.Client/wwwroot/js/index.js
+++ DMS.Client/wwwroot/js/index.js
@@ -1,16 +1,17 @@
 const JS_FILE_PATH = [
     'lib/echarts/echarts.min.js',
     'lib/bootstrap/js/bootstrap.min.js',
-    'js/ChartJsCore.min.js',
+    'js/EChartJsCore.js',
 ]
 
 const CSS_FILE_PATH = [
+    "css/fonts.css",
+    "css/base.css",
     "DMS.Client.styles.css",
-    "css/app.min.css",
-    "lib/bi/font/bootstrap-icons.min.css",
     "lib/bootstrap/css/bootstrap.min.css",
+    "lib/bi/font/bootstrap-icons.min.css",
+    "css/blazor-loading.css",
 ]
 
 CSS_FILE_PATH.forEach(file => document.write(`<link rel="stylesheet" href="${file}" />`))
 JS_FILE_PATH.forEach(file => document.write(`<script src="${file}"></script>`))
-
