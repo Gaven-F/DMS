675b640e6426bd6324d683d4924a017e947e8965
GavenF <GavenF2023@outlook.com>
2024-03-19 18:15:34 +0800

    feat(CLIENT): 项目瘦身，样式更改
    tailwindcss预设样式与antd的部分样式有冲突问题，解决方法已找到，待执行与验证。

--- DMS.Client/postcss.config.js
+++ DMS.Client/postcss.config.js
@@ -1,6 +1,19 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+	plugins: {
+		tailwindcss: {},
+		autoprefixer: {},
+		// FIXME 修复tailwindcss预检样式与antd的样式冲突问题
+		/**
+		 * 但是目前还未将包进行引入（除create-next-app自带的包外，未引入任何的包）
+		 *
+		 * 相关链接：
+		 * antdIssues：https://github.com/ant-design/ant-design/issues/38794
+		 * postcss插件：https://github.com/yunsii/postcss-antd-fixes
+		 */
+
+		// "postcss-antd-fixes": {
+		// 	// Support multiple prefixes, if you do not custom antd class name prefix, it's not necessary option.
+		// 	// prefixes: ['vp-antd', 'ant'],
+		// },
+	},
 };
